762429fcee22e85d61af3e2bc9a9dfefebc39766
anyviewww <anyv.iewww@gmail.com>
2024-12-03 14:57:18 +0700

    разбираюсь с alembic и бд, пока ничего не работает

--- alembic/env.py
+++ alembic/env.py
@@ -1,36 +1,33 @@
-# alembic/env.py
 from logging.config import fileConfig
-from sqlalchemy import engine_from_config, pool
-from sqlalchemy import create_engine
+
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
 from alembic import context
-import os
-from dotenv import load_dotenv
+from app.models import *
 
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
+Base = declarative_base()
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-fileConfig(config.config_file_name)
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-from app.models import Base
 target_metadata = Base.metadata
+#target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
-def run_migrations_offline():
+
+def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
     This configures the context with just a URL
@@ -42,7 +39,7 @@ def run_migrations_offline():
     script output.
 
     """
-    url = DATABASE_URL
+    url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -53,14 +50,19 @@ def run_migrations_offline():
     with context.begin_transaction():
         context.run_migrations()
 
-def run_migrations_online():
+
+def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
 
     """
-    connectable = create_engine(DATABASE_URL)
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
 
     with connectable.connect() as connection:
         context.configure(
@@ -69,6 +71,7 @@ def run_migrations_online():
 
         with context.begin_transaction():
             context.run_migrations()
+
 
 if context.is_offline_mode():
     run_migrations_offline()
--- alembic/script.py.mako
+++ alembic/script.py.mako
@@ -1,22 +1,26 @@
-# alembic/script.py.mako
 """${message}
 
 Revision ID: ${up_revision}
-Revises: ${down_revision|comma,n}
+Revises: ${down_revision | comma,n}
 Create Date: ${create_date}
 
 """
+from typing import Sequence, Union
+
 from alembic import op
 import sqlalchemy as sa
+${imports if imports else ""}
 
 # revision identifiers, used by Alembic.
-revision = '${up_revision}'
-down_revision = '${down_revision}'
-branch_labels = None
-depends_on = None
+revision: str = ${repr(up_revision)}
+down_revision: Union[str, None] = ${repr(down_revision)}
+branch_labels: Union[str, Sequence[str], None] = ${repr(branch_labels)}
+depends_on: Union[str, Sequence[str], None] = ${repr(depends_on)}
 
-def upgrade():
+
+def upgrade() -> None:
     ${upgrades if upgrades else "pass"}
 
-def downgrade():
+
+def downgrade() -> None:
     ${downgrades if downgrades else "pass"}
